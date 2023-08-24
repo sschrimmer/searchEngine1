87475a0ae95f29dead54361da23fd8235b761f97
Steven <stevenschrimmer1@gmail.com>
2023-08-24 11:30:39 -0500

    connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1/googlebooks',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
